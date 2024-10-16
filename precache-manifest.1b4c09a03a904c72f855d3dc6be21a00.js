807d73ba075ca8c5a59de44365b7faf10120d3f7
jenish-2209 <jenish.dholakiya@meetanshi.com>
2024-10-16 18:51:33 +0530

    Add files via upload

---
+++ precache-manifest.1b4c09a03a904c72f855d3dc6be21a00.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7acb65ac6bcbb502a9557d12e3036370",
+    "url": "/reactnew/index.html"
+  },
+  {
+    "revision": "dea50ed8616d90e43ca9",
+    "url": "/reactnew/static/css/2.8ff73448.chunk.css"
+  },
+  {
+    "revision": "23bc5695f8f43890a2c4",
+    "url": "/reactnew/static/css/main.6b02d7ff.chunk.css"
+  },
+  {
+    "revision": "dea50ed8616d90e43ca9",
+    "url": "/reactnew/static/js/2.cb6bc85a.chunk.js"
+  },
+  {
+    "revision": "e5a7172ac4614a7d76bf",
+    "url": "/reactnew/static/js/3.c1a7ea53.chunk.js"
+  },
+  {
+    "revision": "23bc5695f8f43890a2c4",
+    "url": "/reactnew/static/js/main.116dde64.chunk.js"
+  },
+  {
+    "revision": "00684289ec498b40a49d",
+    "url": "/reactnew/static/js/runtime~main.c6c3e700.js"
+  },
+  {
+    "revision": "56c8d80832e37783f12c05db7c8849e2",
+    "url": "/reactnew/static/media/fa-brands-400.56c8d808.ttf"
+  },
+  {
+    "revision": "715d593456fa02fe72a008a72398f5be",
+    "url": "/reactnew/static/media/fa-brands-400.715d5934.woff2"
+  },
+  {
+    "revision": "2d89b49ac28614e9ccd9c81308b5120c",
+    "url": "/reactnew/static/media/fa-regular-400.2d89b49a.woff2"
+  },
+  {
+    "revision": "370dd5af19f8364907b6e2c41f45dbbf",
+    "url": "/reactnew/static/media/fa-regular-400.370dd5af.ttf"
+  },
+  {
+    "revision": "237f4a0afbdb652fb2330ee7e1567dd3",
+    "url": "/reactnew/static/media/fa-solid-900.237f4a0a.woff2"
+  },
+  {
+    "revision": "adec7d6f310bc577f05e8fe06a5daccf",
+    "url": "/reactnew/static/media/fa-solid-900.adec7d6f.ttf"
+  },
+  {
+    "revision": "38fec6307db2547752379b51322ded10",
+    "url": "/reactnew/static/media/fa-v4compatibility.38fec630.ttf"
+  },
+  {
+    "revision": "7c428138d15b872f193c40828f6fa0d3",
+    "url": "/reactnew/static/media/fa-v4compatibility.7c428138.woff2"
+  },
+  {
+    "revision": "43b804084f139409642487f2eecfb1fa",
+    "url": "/reactnew/static/media/pattern-image-cta-bg.43b80408.png"
+  }
+]);
